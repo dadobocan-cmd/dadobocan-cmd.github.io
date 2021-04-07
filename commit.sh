1a7c43545c8f76c1f2289de4f7462dec02289de1
DavidDobocan1 <>
2021-04-07 19:40:08 +0000

    commit message

---
+++ commit.sh
@@ -0,0 +1,7 @@
+git init
+git add --all
+git config --global user.name "dadobocan=cmd"
+git config --global user.email "insrt.r6@gmail.com"
+git commit -m "commit message"
+git remote add origin https://github.com/dadobocan-cmd/portfolio12.github.io.git
+git push -u origin master
